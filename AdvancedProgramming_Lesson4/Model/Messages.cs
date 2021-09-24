424c8a47ab5fbe53fe832f7543f6561168aa6478
Marcin Antonik <marcin.antonik91@gmail.com>
2021-09-24 20:03:52 +0200

    Update

--- AdvancedProgramming_Lesson4/Model/Messages.cs
+++ AdvancedProgramming_Lesson4/Model/Messages.cs
@@ -1,6 +1,7 @@
 ﻿using AdvancedProgramming_Lesson4.Data;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -18,8 +19,11 @@ namespace AdvancedProgramming_Lesson4.Model
         
         public int Id { get; set; }
 
+        [Display(Name = "Użytkownik")]
         public string User { get; set; }
+        [Display(Name = "Wiadomość")]
         public string Message { get; set; }
+        [Display(Name = "Login")]
         public string IsLogin { get; set; }
     }
 }
